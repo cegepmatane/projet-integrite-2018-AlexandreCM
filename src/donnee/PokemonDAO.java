145a78afb537c47cad6ebdffc5be5efa69064f76
AlexandreCM <alexandre.mezeray@gmail.com>
2018-09-12 22:24:35 -0400

    Connexion a la base de donnees

--- src/donnee/PokemonDAO.java
+++ src/donnee/PokemonDAO.java
@@ -1,5 +1,7 @@
 package donnee;
 
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 import modele.Pokemon;
@@ -17,6 +19,23 @@ public class PokemonDAO {
 	}
 	
 	public ArrayList<Pokemon> listePokemon() {
+		String BASEDEDONNEES_DRIVER = "org.postgresql.Driver";
+		String BASEDEDONNEES_URL = "jdbc:postgresql://localhost:5432/pokemon";
+		String BASEDEDONNEES_USAGER = "postgres";
+		String BASEDEDONNEES_MOTDEPASSE = "root";
+		
+		try {
+			Class.forName(BASEDEDONNEES_DRIVER);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			DriverManager.getConnection(BASEDEDONNEES_URL, BASEDEDONNEES_USAGER, BASEDEDONNEES_MOTDEPASSE);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
 		return this.simulerListePokemon();
 	}
 }
